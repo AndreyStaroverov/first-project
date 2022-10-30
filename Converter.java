3142f823c11fc123fd53ee8e1d0ccdca202e6965
Andrey <fanatSai@yandex.ru>
2022-10-30 12:29:07 +0300

    34

---
+++ Converter.java
@@ -0,0 +1,5 @@
+public class Converter {
+	public void main(String[] args) {
+	System.out.println("Convert valuts");
+	}
+}
--- Praktikum.java
+++ Praktikum.java
@@ -1,5 +1,6 @@
 public status class Praktikum {
  public status void main(String[] args){
 System.out.println("Hello bitches?");
+System.out.println("first time ever");
 	}
 }
